ad9df6e872a4812ada14e05a93d8cb8ceb6e59c1
Fabio <88926584+FabioXVI@users.noreply.github.com>
2024-02-07 11:42:50 +0000

    started the navbar and pages

---
+++ app/main/page.tsx
@@ -0,0 +1,15 @@
+import "./main.css";
+export default function Main() {
+  return (
+    // <main className={styles.main}>
+    //   <div className={styles.description}>
+        <p>
+          Main Page ELANCO
+        </p>
+        // <div>
+        // </div>
+    //   </div>
+
+    // </main>
+  );
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,15 @@
+import "./register.css";
+export default function Register() {
+  return (
+    // <main className={styles.main}>
+    //   <div className={styles.description}>
+        <p>
+          Register Page ELANCO
+        </p>
+        // <div>
+        // </div>
+    //   </div>
+
+    // </main>
+  );
+}
