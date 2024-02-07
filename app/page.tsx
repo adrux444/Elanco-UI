e6e5e8acf6d6eba3abfb69f1161b09631032ec6b
Fabio <88926584+FabioXVI@users.noreply.github.com>
2024-02-07 12:43:13 +0000

    Removed underline text-decoration

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Logo from "next/image";
-
+import NavBar from "./navbar/page";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-      <Image src="/images/logo.png" alt="Logo" width={200} height={100} /> {/* Adjust width and height according to your logo dimensions */}
-        <p>
-          Home Page ELANCO
-        </p>
+    <main>
         <div>
+          <NavBar/>
         </div>
-      </div>
 
+        <div>
+          <p>
+            Home Page ELANCO
+          </p>
+        </div>
     </main>
   );
 }
