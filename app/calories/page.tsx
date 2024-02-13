aa3718b5d2f8edda8c799645541b2af84ac2c34c
adrux444 <rachel.dixon10@icloud.com>
2024-02-13 12:20:41 +0000

    Edited main page + made pages to display further information

---
+++ app/calories/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "../footer/page";
+import NavBar from "../navbar/page";
+import './calories.css';
+
+export default function Login() {
+  return (
+    <main>
+        <div>
+          <NavBar/>
+          </div>
+          <div> <h1> Calories Page </h1></div>
+          <div>
+            <Footer/>
+          </div>
+
+    </main>
+  );
+}
---
+++ app/water/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "../footer/page";
+import NavBar from "../navbar/page";
+import './water.css';
+
+export default function Login() {
+  return (
+    <main>
+        <div>
+          <NavBar/>
+          </div>
+          <div> <h1> Water Page </h1></div>
+          <div>
+            <Footer/>
+          </div>
+
+    </main>
+  );
+}
