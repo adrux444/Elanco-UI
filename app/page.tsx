c18ba260bb7515ce27291c722ac226e32a276fa8
adrux444 <rachel.dixon10@icloud.com>
2024-02-13 10:53:15 +0000

    Re-add home and about again

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,48 @@
 import NavBar from "./navbar/page";
-import "./page.css"
+import Button from '@mui/joy/Button';
+import Stack from "@mui/joy/stack";
+import Image from "next/image"
+import Link from 'next/link'
+import "./home.css";
 
 export default function Home() {
   return (
     <main>
         <div>
           <NavBar/>
+          </div>
           <div>
-          <p>
-            
-          </p>
+          </div>
+          <form>
+          <div className="info-container">
+            <h1>Welcome</h1>
+            <h4>Welcome to Elanco's Animal Insights Portal! Here, we empower you with the knowledge to better understand and care for your beloved animals. Our platform utilizes advanced data analytics to provide you with comprehensive statistics about your pets or livestock. Through visually engaging graphs and informative text, we offer insights into various aspects of your animals' health and behavior. Whether you're a pet owner looking to monitor your furry friend's activity levels or a farmer seeking to optimize your herd's performance, our website is your go-to destination for meaningful data representation. Explore the fascinating world of animal insights with us and make informed decisions for the well-being of your cherished companions.</h4>
+            <br/>
+          </div>
+        </form>
+        <br/>
+        <div className="images-container">
+          <Image className="image" src="/images/dog1.jpg" alt="dog1" width={294.4} height={196.4} style={{objectFit:"contain"}}/>
+          <Image className="image" src="/images/dog2.jpg" alt="dog2" width={294.4} height={196.4} style={{objectFit:"contain"}}/>
+          <Image className="image" src="/images/dog3.jpg" alt="dog3" width={294.4} height={196.4} style={{objectFit:"contain"}}/>
         </div>
+        <div className="login-container">
+          <h3>Login or Sign Up now to find out more!</h3>
+          <div className="center">
+          
+              <Stack direction="row" spacing={2} >
+                
+                <Link href="/login" passHref>
+                  <Button target="_blank">Login</Button>
+                </Link>
+                <Link href="/register" passHref>
+                  <Button target="_blank">Sign Up</Button>
+                </Link>
+              </Stack>
+          </div>
+          
         </div>
     </main>
   );
 }
+
