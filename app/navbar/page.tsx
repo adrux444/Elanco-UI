feeba173f60550d6dd7e1d8d0952a18d8be949d0
Fabio <88926584+FabioXVI@users.noreply.github.com>
2024-02-15 12:44:27 +0000

    removed the gaps in login and home pages

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link"
-import Image from "next/image"
 import "./navbar.css"
+import Image from "next/image"
+import Link from "next/link"
 export default function Nav(){
-
     return (
         <div  className="container">
             <div className="link-container" id="navbar">
                 <Image className="elanco-logo" src="/images/cropfblogo.jpg" alt="Logo" width={150} height={50}/>
-                <Link className="redirect" href='../'><p>Home</p></Link>
+                <Link className="redirect" href='/'><p>Home</p></Link>
                 <Link className="redirect" href='/login'><p>Login</p></Link>
                 <Link className="redirect" href='/register'><p>Sign Up</p></Link>
                 <Link className="redirect" href='/about'><p>About</p></Link>
