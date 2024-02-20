4ad7a0489081fe78c5a1e9ee299acc4643e1d579
Fabio <88926584+FabioXVI@users.noreply.github.com>
2024-02-20 15:06:40 +0000

    Worked on Breathing, Temperature, Weight and Heart page

--- app/breathing/page.tsx
+++ app/breathing/page.tsx
@@ -1,23 +1,65 @@
 "use client";
-import Footer from "../footer/page";
 import NavBar from "../navbar/page";
+import Footer from "../footer/page";
 import './breathing.css';
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts';
+import { useState } from "react";
+import axios from "axios";
+interface DataItem {
+  Id: number;
+  average_weight: number;
+}
 
-export default function Login() {
+export default function Breathing() {
+
+  const [data, setData] = useState<DataItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [chartData, setChartData] = useState();
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<DataItem[]>('http://localhost:4000/average');
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>
+
   return (
     <main>
         <div>
           <NavBar/>
           </div>
           <div> <h1> Breathing Rate Page </h1>
-            <BarChart
-              xAxis={[{ scaleType: 'band', data: ['Canine 1', 'Canine 2', 'Canine 3'] }]}
-              series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-              width={500}
-              height={300}
-            />
+            {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>{error}</p>
+          ) : (
+            <ul>
+              {data.map((item) => (
+                <li key={item.Id}>
+                  <BarChart
+                    xAxis={[{ scaleType: 'band', data: ['Canine 1', 'Canine 2', 'Canine 3'] }]}
+                    series={[{ data: [item.average_weight, 3, 5]}]}
+                    width={500}
+                    height={300}
+                  />
+              </li>
+            ))}
+          </ul>
+        )}
           </div>
           <div>
             <Footer/>
--- app/heart/page.tsx
+++ app/heart/page.tsx
@@ -1,28 +1,67 @@
 "use client";
-import Footer from "../footer/page";
 import NavBar from "../navbar/page";
+import Footer from "../footer/page";
 import './heart.css';
 import * as React from 'react';
 import { LineChart } from '@mui/x-charts';
+import { useState } from "react";
+import axios from "axios";
+interface DataItem {
+  Id: number;
+  average_value_heart_rate: number;
+}
 
-export default function Login() {
+export default function Heart() {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [chartData, setChartData] = useState();
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<DataItem[]>('http://localhost:4000/average');
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>
   return (
     <main>
+      <div>
+        <NavBar/>
+        </div>
+        <div> <h1> Heart Rate Page </h1>
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>{error}</p>
+          ) : (
+            <ul>
+              {data.map((item) => (
+              <li key={item.Id}>
+                <LineChart
+                  xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                  series={[{data: [item.average_value_heart_rate, 8.5, 1.5, 5],color: '#e15759',curve: "linear"},]}
+                  width={500}
+                  height={300}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+        </div>
         <div>
-          <NavBar/>
-          </div>
-          <div> <h1> Heart Rate Page </h1>
-            <LineChart
-              xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-              series={[{data: [2, 5.5, 2, 8.5, 1.5, 5],color: '#e15759',curve: "linear"},]}
-              width={500}
-              height={300}
-            />
-          </div>
-          <div>
-            <Footer/>
-          </div>
-
+          <Footer/>
+        </div>
     </main>
   );
 }
