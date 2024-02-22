447d1f4ae71010d09ac4bf007a4ea7c98dd1639c
adrux444 <rachel.dixon10@icloud.com>
2024-02-22 12:03:56 +0000

    add link to main page by clicking on Elanco Logo

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,11 +1,17 @@
 import "./navbar.css"
 import Image from "next/image"
-import Link from "next/link"
+import { Link } from "@mui/material"
 export default function Nav(){
+    const currentUrl = window.location.href;
+    const urlObj = new URL(currentUrl);
+    let dogNum = urlObj.searchParams.get('dog')
+    const link = `/main?dog=${dogNum}`;
     return (
         <div  className="container">
             <div className="link-container" id="navbar">
-                <Image className="elanco-logo" src="/images/cropfblogo.jpg" alt="Logo" width={150} height={50}/>
+                <Link href = {link.toString()}>
+                    <Image className="elanco-logo" src="/images/cropfblogo.jpg" alt="Logo" width={150} height={50}/>
+                </Link>
                 <Link className="redirect" href='/'><p>Home</p></Link>
                 <Link className="redirect" href='/login'><p>Login</p></Link>
                 <Link className="redirect" href='/register'><p>Sign Up</p></Link>
